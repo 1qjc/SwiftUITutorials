a8cc628c037f6115dc451d95051b6c79fb26c446
1qjc <102451300+1qjc@users.noreply.github.com>
2025-06-04 14:39:07 +0800

    Views, structures, and properties
    ForecastView

---
+++ theTutorial/ForecastView.swift
@@ -0,0 +1,62 @@
+//
+//  ForecastView.swift
+//  theTutorial
+//
+//  Created by 1qjc on 6/4/25.
+//
+
+import SwiftData
+import SwiftUI
+
+struct ForecastView: View {
+  var body: some View {
+
+    HStack {
+      DayForecast(day: "Mon", isRainy: false, high: 70, low: 50)
+      DayForecast(day: "Tue",isRainy: true, high: 60, low: 40)
+    }
+
+  }
+}
+#Preview {
+  ForecastView()
+}
+
+struct DayForecast: View {
+  let day: String
+  let isRainy: Bool
+  let high: Int
+  let low: Int
+  
+  var iconName: String {
+    if isRainy {
+      return "cloud.rain.fill"
+    } else {
+      return "sun.max.fill"
+    }
+  }
+  var iconColor: Color {
+    if isRainy {
+      return Color.blue
+    } else {
+      return Color.yellow
+    }
+  }
+  
+  var body: some View {
+    VStack {
+      Text(day)
+        .font(.headline)
+      Image(systemName: iconName)
+        .foregroundColor(iconColor)
+        .font(.largeTitle)
+        .padding(5)
+      Text("High: \(high)")
+        .fontWeight(.semibold)
+      Text("Low: \(low)")
+        .fontWeight(.medium)
+        .foregroundStyle(Color.secondary)
+    }
+    .padding()
+  }
+}
--- theTutorial/theTutorialApp.swift
+++ theTutorial/theTutorialApp.swift
@@ -5,28 +5,14 @@
 //  Created by 1qjc on 6/4/25.
 //
 
-import SwiftUI
 import SwiftData
+import SwiftUI
 
 @main
 struct theTutorialApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-        .modelContainer(sharedModelContainer)
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
